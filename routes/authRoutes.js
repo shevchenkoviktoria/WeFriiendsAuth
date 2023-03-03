3d75d1b65ecda379a4922f8e6e38f82e30412dab
natasobl <natasobl@hotmail.com>
2023-03-03 17:17:46 -0500

    fix

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -54,6 +54,12 @@ module.exports = (app) => {
         })
     });
 
+    const authenticated = (req,res,next)=>{
+        const customError = new Error('you are not logged in');
+        customError.statusCode = 401;
+        (!req.user) ? next(customError) : next()
+    }
+
     app.get("/api/auth/login/success", authenticated, (req,res) => {
         console.log("in login success ", req._user, req.user)
         if (req.user) {
@@ -73,11 +79,7 @@ module.exports = (app) => {
         }
     });
 
-    const authenticated = (req,res,next)=>{
-        const customError = new Error('you are not logged in');
-        customError.statusCode = 401;
-        (!req.user) ? next(customError) : next()
-    }
+    
     
     app.get("/api/auth/google/callback", passport.authenticate('google', {
         successRedirect: 'http://localhost:3000', // "/api/auth/login/success",
