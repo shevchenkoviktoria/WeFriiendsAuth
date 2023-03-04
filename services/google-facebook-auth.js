708d0811a884e6a473f51cabba792af9ab34c1a5
natasobl <natasobl@hotmail.com>
2023-03-04 01:28:44 -0500

    Fix

--- services/google-facebook-auth.js
+++ services/google-facebook-auth.js
@@ -23,7 +23,7 @@ passport.use(
         clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
         clientSecret: process.env.GOOGLE_AUTH_SECRET,
         callbackURL: "https://clumsy-glasses-clam.cyclic.app/api/auth/google/callback",
-        passReqToCallback: true
+       // passReqToCallback: true
     },
      async (accessToken, refreshToken, profile, done) => {
         console.log("profile ", profile)
