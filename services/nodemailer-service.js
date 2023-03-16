26172e56f915d30bbbbe252011837f30e4464093
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2023-03-16 15:36:31 -0400

    Update nodemailer-service.js

--- services/nodemailer-service.js
+++ services/nodemailer-service.js
@@ -44,7 +44,7 @@ const transporter = nodemailer.createTransport({
         clientId: process.env.GMAIL_CLIENT_ID,
         clientSecret: process.env.GMAIL_CLIENT_SECRET,
         refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-        accessToken
+        accessToken: accessToken()
     },
 })
 return transporter;
