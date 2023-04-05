668fa66397befc271ae11c1f43c5721fba228aba
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2023-04-05 02:14:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ const HTTP_PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(
     cors(
-//     {origin: "http://localhost:3000",
-//     credentials: true
-// }
+    {origin: "http://localhost:3000",
+   // credentials: true
+}
 )
 );
 app.use(express.static('public'))
