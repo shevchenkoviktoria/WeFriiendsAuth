36c37bae2f60bab57fc8312e80ceeea2aeb36451
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2023-04-06 01:00:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ dotenv.config();
 const passport = require("passport");
 const mongoose = require("mongoose");
 const oneDay = 1000 * 60 * 60 * 24;
-const session = require('express-session')
-app.use(session({
+const expressSession = require('express-session')
+const session = {
     secret: "secret",
     resave: false ,
     saveUninitialized: false ,
     cookie: { maxAge: 60 * 60 * 1000 }
-}));
+}
+
+if (app.get("env") === "production") {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true; 
+}
+
 
 const HTTP_PORT = process.env.PORT || 8080;
 
@@ -24,15 +30,18 @@ app.use(
 }
 )
 );
-app.use(express.static('public'))
+//app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
+app.use(expressSession(session));
 
 app.use(passport.initialize());
 app.use(passport.session());
 require("./models/User");
 require("./services/google-facebook-auth");
 require("./services/email-auth");
-require("./routes/authRoutes")(app);
 
+require("./routes/authRoutes")(app);
 mongoose.connect(
     "mongodb+srv://wefriiends-backup:wefriiends2023@cluster0.wir50id.mongodb.net/authorization?retryWrites=true&w=majority",
     { 
