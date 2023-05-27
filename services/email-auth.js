ef328917420d2212905e58f5b2414d44905bbea7
natasobl <natasobl@hotmail.com>
2023-05-26 20:20:22 -0400

    Added token

--- services/email-auth.js
+++ services/email-auth.js
@@ -3,90 +3,88 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("./nodemailer-service.js");
 let User = mongoose.model("users");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
-module.exports.registerUser = async(userData, req,res) => {
-    console.log("in register user")
-    if (userData.password !== userData.password2) {
-        res.send('Passwords do not match');
-    }
-    const hashedPassword = await bcrypt.hash(userData.password, 10);
-  //  userData.password = hash;
-//     const token = jwt.sign(
-//         { userId: userData.email },
-//         process.env.JWT_SECRET
-//     );
-    const confirmationCode = uuidv4();
-    console.log("code is ", confirmationCode)
-     const userToSave = new User({
+module.exports.registerUser = async (userData, req, res) => {
+  console.log("in register user");
+  if (userData.password !== userData.password2) {
+    res.send("Passwords do not match");
+  }
+  const hashedPassword = await bcrypt.hash(userData.password, 10);
+  const confirmationCode = uuidv4();
+  const userToSave = new User({
     userId: userData.email,
     password: hashedPassword,
-    confirmationCode
+    confirmationCode,
   });
-    console.log("user ", userToSave)
-   
-    const response = await saveUser(userToSave, confirmationCode);
-    return response;
-}
+  console.log("user ", userToSave);
 
-const saveUser = async(user, token) => {
-    console.log('in save')
-    try {
-        const result = await user.save();
-        if (result) {
-            await nodemailer.sendConfirmationEmail(result.userId, token);
-            return (`Pending registration confirmation for ${result.userId}`);
-        }
-    } catch(err) {
-        if (err.code === 11000) {
-           return ("This email address is already associated with an account");
-        } 
-        return(`There was an error in sending confirmation email ${err.code}`);
+  const response = await saveUser(userToSave, confirmationCode);
+  return response;
+};
+
+const saveUser = async (user, token) => {
+  try {
+    const result = await user.save();
+    if (result) {
+      await nodemailer.sendConfirmationEmail(result.userId, token);
+      return `Pending registration confirmation for ${result.userId}`;
     }
-}
+  } catch (err) {
+    if (err.code === 11000) {
+      return "This email address is already associated with an account";
+    }
+    return `There was an error in sending confirmation email ${err.code}`;
+  }
+};
 
 module.exports.checkUser = (userData) => {
-    return new Promise(function (resolve, reject) {
-      User.findOne({ userId: userData.email })
-        .exec()
-        .then((user) => {
-          bcrypt.compare(userData.password, user.password).then((res) => {
-            if (user.status != "Active") {
-              reject(
-                "Pending Account. Please verify your email to gain access to your profile"
-              );
-            }
-            if (res === true) {
-              resolve(user);
-            } else {
-              reject("Incorrect password for user " + userData.email);
-            }
-          });
-        })
-        .catch((err) => {
-          reject("Unable to find user " + userData.email);
+  return new Promise(function (resolve, reject) {
+    User.findOne({ userId: userData.email })
+      .exec()
+      .then((user) => {
+        bcrypt.compare(userData.password, user.password).then((res) => {
+          if (user.status != "Active") {
+            reject(
+              "Pending Account. Please verify your email to gain access to your profile"
+            );
+          }
+          if (res === true) {
+            resolve(user);
+          } else {
+            reject("Incorrect password for user " + userData.email);
+          }
         });
-    });
-  };
-  
+      })
+      .catch((err) => {
+        reject("Unable to find user " + userData.email);
+      });
+  });
+};
+
 module.exports.verifyUserEmail = (confirmationCode) => {
-    return new Promise(function (resolve, reject) {
-        User.findOne({
-        confirmationCode: confirmationCode,
-        })
-        .then((user) => {
-            if (!user) {
-                reject("User Not found.");
-            }
-            user.status = "Active";
-            user.save((err) => {
-            if (err) {
-                reject({ message: err });
-            } else {
-                resolve("Account has been activated");
-            }
-            });
-        })
-        .catch((err) => reject(err));
-    });
+  return new Promise(function (resolve, reject) {
+    User.findOne({
+      confirmationCode: confirmationCode,
+    })
+      .then((user) => {
+        if (!user) {
+          reject("User Not found.");
+        }
+        const payload = {
+          _id: user._id,
+          userId: user.userId,
+        };
+        const token = jwt.sign(payload, process.env.JWT_SECRET);
+        user.status = "Active";
+        user.save((err) => {
+          if (err) {
+            reject({ message: err });
+          } else {
+            resolve({ message: "Account has been activated", token });
+          }
+        });
+      })
+      .catch((err) => reject(err));
+  });
 };
