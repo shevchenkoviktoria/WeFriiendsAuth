019e9f50e238238173e54b268dca80bd93d5e9fc
natasobl <natasobl@hotmail.com>
2023-05-30 17:47:04 -0400

    Add route

--- routes/updatePassword.js
+++ routes/updatePassword.js
@@ -2,7 +2,7 @@ const updatePassword = require ("../services/updatePassword");
 
 module.exports = (app) => {
  
-  app.patch("api/auth/updatePassword", (req, res) => {
+  app.patch("/api/auth/updatePassword", (req, res) => {
     console.log(" in route")
     updatePassword(req.body);
   });
