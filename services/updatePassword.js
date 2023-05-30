35cac3dc6bb5bac0f579ddece11ad1a50bb3d6c2
natasobl <natasobl@hotmail.com>
2023-05-30 18:10:21 -0400

    Fix

--- services/updatePassword.js
+++ services/updatePassword.js
@@ -3,20 +3,20 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 let User = mongoose.model("users");
 
-const updatePassword = async (userData, req, res) => {
-    console.log("in update password")
-  const { email, password1, password2 } = userData;
-  console.log(email, password1, password2)
+const updatePassword = async (userData, res) => {
+  console.log("in update password");
+  const { email, password, password2 } = userData;
+  console.log(email, password, password2);
   const user = await User.findOne({ userId: email });
   if (!user) {
     return res.status(400).send("User doesn't exist");
   }
-  if (password1 !== password2) {
+  if (password !== password2) {
     return res
       .status(400)
       .json({ success: false, msg: "Passwords do not match" });
   }
-  const hashedPassword = await bcrypt.hash(password1, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
   const token = jwt.sign(
     { userId: email },
     "secret" // process.env.JWT_SECRET
@@ -46,4 +46,4 @@ const updateUser = async (updatedUser) => {
   }
 };
 
-module.exports = {updatePassword};
+module.exports = { updatePassword };
