7b521c91989181a3e063caad39939ea8c4118c0c
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2023-06-12 23:46:12 -0400

    Update google-facebook-auth.js

--- services/google-facebook-auth.js
+++ services/google-facebook-auth.js
@@ -6,8 +6,8 @@ const User = mongoose.model("users");
 
 passport.use(
     new GoogleStrategy({
-        clientID: '681911775744-38tdbjb6qsrejqre35ce5ems8sg7hnje.apps.googleusercontent.com',  //process.env.GOOGLE_AUTH_CLIENT_ID,
-        clientSecret: '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',  //process.env.GOOGLE_AUTH_SECRET,
+        clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_AUTH_SECRET,
         callbackURL: "https://clumsy-glasses-clam.cyclic.app/api/auth/google/callback",      
     },
     async (accessToken, refreshToken, profile, done) => {       
