1a60d3d9d5bd0da25f50dd2d06fd2fc0b56e9e8a
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2023-06-23 19:04:17 -0400

    Update google-facebook-auth.js

--- services/google-facebook-auth.js
+++ services/google-facebook-auth.js
@@ -8,7 +8,8 @@ passport.use(
     new GoogleStrategy({
         clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
         clientSecret: process.env.GOOGLE_AUTH_SECRET,
-        callbackURL: "https://clumsy-glasses-clam.cyclic.app/api/auth/google/callback",      
+        callbackURL: "https://clumsy-glasses-clam.cyclic.app/api/auth/google/callback",
+        passReqToCallback   : true
     },
     async (accessToken, refreshToken, profile, done) => {       
         console.log('in strategy')
