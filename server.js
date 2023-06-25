6f3bbafb4b754632382d61de7ee876637921c308
natasobl <natasobl@hotmail.com>
2023-06-25 14:59:57 -0400

    Add cookie

--- server.js
+++ server.js
@@ -8,22 +8,21 @@ const passport = require("passport");
 const mongoose = require("mongoose");
 const oneDay = 1000 * 60 * 60 * 24;
 //const expressSession = require('express-session')
-var cookieSession = require('cookie-session')
+var cookieSession = require('cookie-session');
 const session = {
-    name: 'session',
-    keys: ['secret'],
-    maxAge: 60*60*1000, 
-    // secret: "secret",
-    // resave: true ,
-    // saveUninitialized: true ,
-    // cookie: { 
-    //     maxAge: 60*60*1000, 
-    //    sameSite: 'none',
-    //secure: false
+    // name: 'session',
+    // keys: ['secret'],
+    // maxAge: 60*60*1000, 
+   secret: 'secret',
+   cookie: {}
 }
 
 
 //app.enable('trust proxy');
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+ // sess.cookie.secure = true // serve secure cookies
+}
 
 const HTTP_PORT = process.env.PORT || 8080;
 
@@ -38,7 +37,7 @@ app.use(
 
 //app.use(expressSession(session));
 app.use(cookieSession(session));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 require("./models/User");
