5c7264607e7498428b74cd0a438a6725de188bce
ViktoriaSharpershape <viktoriia.shevchenko@sharpershape.com>
2024-03-26 11:54:33 +0200

    added file with import

---
+++ services/cloudinary-service.js
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from 'cloudinary';
+
+
+cloudinary.config({ 
+    cloud_name: 'dvfaj9nyh', 
+    api_key: '274824594938862', 
+    api_secret: 'mswcqE8TuQpqxABBkUj9nojQ22M',
+    API_environment_variable: 'CLOUDINARY_URL=cloudinary://274824594938862:mswcqE8TuQpqxABBkUj9nojQ22M@dvfaj9nyh'
+  });
